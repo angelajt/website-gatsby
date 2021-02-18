cf1a371ae45f36b5344bbd7786be3617058f4578
angelajt <angela@t7a.org>
2021-02-17 20:55:22 -0800

    add table of contents for posts, change type size/scale

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,18 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              ordered: false,
+              fromHeading: 2,
+              toHeading: 6,
+              className: "table-of-contents"
+            },
+          },
+          `gatsby-remark-autolink-headers`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
